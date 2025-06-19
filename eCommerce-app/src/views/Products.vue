84731ada8ddb582702e25ff711d25cf3e732c455
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-06-19 19:49:26 +0300

    layout centered

--- eCommerce-app/src/views/Products.vue
+++ eCommerce-app/src/views/Products.vue
@@ -3,7 +3,18 @@ import ProductCard from "../components/ProductCard.vue";
 </script>
 
 <template>
-  <v-container>
-    <ProductCard />
-  </v-container>
+  <div class="grid-container">
+    <ProductCard v-for="n in 8" :key="n" />
+  </div>
 </template>
+<style scoped>
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr); /* 3 columns */
+  gap: 4rem;
+  padding: 2rem;
+  justify-items: center;
+  align-items: center;
+  /* border: solid red; */
+}
+</style>
