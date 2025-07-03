2ba0fc9d5796858b28012d2c430ad9ad07d33a90
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-07-03 06:00:13 +0300

    pinia stores (products, cart) ara created

---
+++ eCommerce-app/src/shared/store/pinina/modules/cart.ts
@@ -0,0 +1,35 @@
+// stores/cart.ts
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import type Product from "@/data/entities/Product";
+
+export const useCartStore = defineStore(
+  "cart",
+  () => {
+    const items = ref<Product[]>([]);
+
+    const addToCart = (product: Product) => {
+      console.log("Adding product to cart from store:", product);
+      items.value.push(product);
+    };
+
+    const removeFromCart = (productId: number) => {
+      console.log("Removing product from cart:", productId);
+      items.value = items.value.filter((item) => item.id !== productId);
+    };
+
+    const cartItems = computed(() => items.value);
+    const cartCount = computed(() => items.value.length);
+
+    return {
+      items,
+      addToCart,
+      removeFromCart,
+      cartItems,
+      cartCount,
+    };
+  },
+  {
+    persist: true, // Enable persistence for the entire cart store
+  }
+);
